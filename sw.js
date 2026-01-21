fe817e011a706d18bd338f79c7f622feaf61003f
CLENA TV <tvclena@gmail.com>
2026-01-21 00:30:31 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener("install",e=>{
+  e.waitUntil(
+    caches.open("admin-v1").then(c=>
+      c.addAll(["./","index.html","manifest.json"])
+    )
+  );
+});
+
+self.addEventListener("fetch",e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  );
+});
