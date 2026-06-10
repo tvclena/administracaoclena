8497316035bcc34461362701ad530bd780f213bc
CLENA TV <tvclena@gmail.com>
2026-06-10 13:09:50 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,17 @@
-const CACHE = "painel-admin-v8";
+const CACHE = "painel-admin-v10";
 const FILES = [
   "./",
   "./index.html",
+  "./profile.html",
+  "./painel-tipo-loja.html",
+  "./config.html",
+  "./admin-customizacoes.html",
   "./manifest.json"
+
+
+
+
+  
 ];
 
 self.addEventListener("install", e=>{
