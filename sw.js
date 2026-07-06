b2d94941c5ca3bb072238a68b1e52144d8744965
CLENA TV <tvclena@gmail.com>
2026-07-05 23:06:13 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,57 @@
-const CACHE = "painel-admin-v20";
+const CACHE = "clena-admin-v21";
+
 const FILES = [
   "./",
   "./index.html",
+
   "./profile.html",
   "./painel-tipo-loja.html",
   "./config.html",
   "./admin-customizacoes.html",
+  "./admin-agendamentos.html",
+  "./admin-relatorios-vendas.html",
+  "./admin-relatorios-uso.html",
   "./linkbio.html",
-  "./manifest.json"
 
-
-
-
-  
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
-self.addEventListener("install", e=>{
-  e.waitUntil(
-    caches.open(CACHE).then(cache=>cache.addAll(FILES))
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(FILES))
+      .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener("fetch", e=>{
-  e.respondWith(
-    caches.match(e.request).then(res=> res || fetch(e.request))
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+
+      return fetch(event.request).then(response => {
+        return response;
+      }).catch(() => {
+        if (event.request.mode === "navigate") {
+          return caches.match("./index.html");
+        }
+      });
+    })
   );
 });
